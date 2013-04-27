c13afc7578771e48a7811740dc5af526f06636c3
Jeff Gregory <jgregory@bitcasa.com>
2013-04-26 17:27:56 -0700

    enpassant king/queen side castels in, need to write test

--- newBoard.py
+++ newBoard.py
@@ -3,12 +3,10 @@
 import ipdb
 
 class InvalidMove(Exception):
-    def __init__(self, msg=""):
-        self.msg = msg
     def __str__(self):
         return self.msg
 
-class Space(object):
+class Square(object):
     #~ Parts of the board that make up the piece location.  The space is needed
     #~ for the linking_spaces method in Board
     letters_numbers = ("abcdefgh  ", "12345678  ")
@@ -47,18 +45,19 @@ class Space(object):
 
     def init(self, board):
         self.board = board
-        self._link_spaces(board)
+        self._link_squares(board)
 
     def move(self, to):
-        try:
-            self.piece.move(self.get_paths(), to)
-        except InvalidMove:
-            return False
-        else:
-            self.piece.square = self.board.__getattr__(to)
-            self.board.__getattr__(to).piece = self.piece
+        move, special = self.piece.move(self.get_paths(), to)
+
+        if move:
+            self.piece.square = self.board.get(to)
+            self.board.get(to).piece = self.piece
             self.piece = Empty(self.location)
-            self.board.show()
+            #~ self.board.show()
+            return move, special
+        else:
+            return move, special
 
     def get_paths(self):
         """Get all the squares that have access to attack this square."""
@@ -76,32 +75,32 @@ class Space(object):
         direction passed."""
         current_color = self.piece.color
         while True:
-            space = getattr(self, direction)
+            square = getattr(self, direction)
             #~ None implies we're at the edge of the board
-            if space is None:
+            if square is None:
                 break
 
             #~ colors match meaning we're attacking our own piece.  Only save up to the
             #~ piece and not the piece as we can't capture.
-            if (current_color != "wb".replace(space.piece.color, "")) and space.piece.color:
+            if (current_color != "wb".replace(square.piece.color, "")) and square.piece.color:
                 return path
 
             #~ colors are different meaning we're attaching a valid piece. Save up to and
             #~ including the piece first encountered.
-            elif (current_color == "wb".replace(space.piece.color, "")) and space.piece.color:
-                path.add(space)
+            elif (current_color == "wb".replace(square.piece.color, "")) and square.piece.color:
+                path.add(square)
                 return path
 
-            path.add(space)
+            path.add(square)
             #~ L shape moves only propagate one space for every direction.
             if direction.startswith("_L"):
                 return path
 
             #~ Set the neighbor as the current square.
-            self = space
+            self = square
         return path
 
-    def _link_spaces(self, board):
+    def _link_squares(self, board):
         """Finds all the neighboring squares to this square and sets an attr
         to the neighboring object."""
         for direction in self.compus_directions:
@@ -132,37 +131,43 @@ class Piece(object):
               "Pawn"  : {"b": "♙", "w": "♟"},
               "Empty" : {"" : " "}}
 
-    def __init__(self, square, color=""):
+    def __init__(self, square, color="", side=""):
         self.move_count = 0
         self.square     = square
         self.color      = color
         self.name       = self.__class__.__name__
         self.piece      = self.pieces[self.name][self.color]
+        self.side       = side
 
     def __str__(self):
         return self.piece
 
-    def move(self, paths, to):
+    def move(self, paths, to):        
         from_letter, from_number = self.square.location
         to_letter, to_number = to
 
         dx = "abcdefgh".find(to_letter) - "abcdefgh".find(from_letter)
         dy = "12345678".find(to_number) - "12345678".find(from_number)
 
-        return self._move(paths, to, dx, dy)
+        vpos = map(lambda x: x.location, paths["V"])
+        dpos = map(lambda x: x.location, paths["D"])
+        hpos = map(lambda x: x.location, paths["H"])
+        lpos = map(lambda x: x.location, paths["L"])
+        
+        return self._move(paths, to, dx, dy, vpos, dpos, hpos, lpos)
 
 
 class Empty(Piece):
     def __init__(self, square):
         Piece.__init__(self, square)
 
-    def _move(self, *args):
+    def move(self, *args):
         raise InvalidMove("Must select a square with a piece on it")
 
 
 class Pawn(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
         #~ The direction that the piece must move, only needed with Pawns
         if self.color == "w":
@@ -172,101 +177,123 @@ class Pawn(Piece):
             #~ Must move down the board
             self.must_move = -1
 
-    def _move(self, paths, to, dx, dy):
-        valid_V_locations = map(lambda x: x.location, paths["V"])
-        valid_D_locations = map(lambda x: x.location, paths["D"])
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        to_square = self.square.board.get(to)
+        #~ Square to handle enpassant
+        special = to_square.__getattribute__(["", "_S", "_N"][self.must_move])
 
         valid_moves = [
         #~ Normal attack
-        ((dy * self.must_move) == 1) and (to in valid_D_locations) and (self.square.board.__getattr__(to).piece.name is not "Empty"),
+        ((dy * self.must_move) == 1) and dpos and (to_square.piece.name is not "Empty"),
         #~ Move one square
-        ((dy * self.must_move) == 1) and (to in valid_V_locations) and (self.square.piece.name is "Empty"),
+        ((dy * self.must_move) == 1) and vpos and (to_square.piece.name is "Empty"),
         #~ Move two squares on first move
-        ((dy * self.must_move) == 2) and (to in valid_V_locations) and (self.move_count == 0),
+        ((dy * self.must_move) == 2) and vpos and (to_square.piece.name is "Empty") and 
+        (self.move_count == 0),
         #~ Enpassant
-        False]
+        ((dy * self.must_move) == 1) and dpos and (special.piece.color != self.color) and 
+        (special.piece.name == "Pawn") and (special.piece.move_count == 1) and 
+        (self.square.board.last_moved == special)
+        ]
 
         if any(valid_moves):
-            return True
-        raise InvalidMove("Invalid Pawn Move")
+            self.move_count += 1
+            if valid_moves[-1]:
+                return True, "enpassant"
+            else:
+                return True, "normal"
+        else:
+            return False, ""
 
 
 class Rook(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
-    def _move(self, paths, to, dx, dy):
-        valid_V_locatoins = map(lambda x: x.location, paths["V"])
-        valid_H_locations = map(lambda x: x.location, paths["H"])
-
-        valid_moves = [
-        (abs(dy) > 1) and (dx == 0) and (to in valid_V_locations),
-        (abs(dx) > 1) and (dy == 0) and (to in valid_H_locations)]
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        valid_moves = [(abs(dy) > 1) and (dx == 0) and (to in vpos),
+                       (abs(dx) > 1) and (dy == 0) and (to in hpos)]
 
         if any(valid_moves):
-            return True
-        raise InvalidMove("Invalid Rook Move")
+            return True, "normal"
+        else:
+            return False, ""
 
 
 class Knight(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
-    def _move(self, paths, to, dx, dy):
-        valid_L_locations = map(lambda x: x.location, paths["L"])
-
-        if to in valid_L_locations:
-            return True
-        raise InvalidMove("Invalid Knight Move")
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        if to in lpos:
+            return True, "normal"
+        else:
+            return False, ""
 
 
 class Bishop(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
-    def _move(self, paths, to, dx, dy):
-        valid_D_locations = map(lambda x: x.location, paths["D"])
-
-        if to in valid_D_locations:
-            return True
-        raise InvalidMove("Invalid Bishop Move")
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        if to in dpos:
+            return True, "normal"
+        else:
+            return False, ""
 
 
 class King(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
-    def _move(self, paths, to, dx, dy):
-        valid_V_locations = map(lambda x: x.location, paths["V"])
-        valid_D_locations = map(lambda x: x.location, paths["D"])
-        valid_H_locations = map(lambda x: x.location, paths["H"])
-
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        
+        ipdb.set_trace()
+        
+        rooks = []
+        for side in "kq":
+            try:
+                r, = filter(lambda x: x.name is "Rook" and x.side is "k", self.square.piece.team)
+            except ValueError:
+                raise Exception("Rook not found in piece team")
+            else:
+                rooks.append(r)
+        
         valid_moves = [
-            ((abs(dx) == 1) or (abs(dy) == 1)) and ((to in valid_V_locations) or
-            (to in valid_H_locations) or (to in valid_D_locations)),]
+            #~ Normal King moves
+            ((abs(dx) == 1) or (abs(dy) == 1)) and ((to in vpos) or (to in hpos) or (to in dpos)),
+            #~ King side castle
+            (dx ==  2) and (dy == 0) and (self.move_count == 0) and (rooks[0].move_count == 0) and 
+            (rooks[0].move_count == 0) and ("g1" in hpos),
+            #~ Queen side castle
+            (dx == -2) and (dy == 0) and (self.move_count == 0) and (rooks[1].move_count == 0) and 
+            (rooks[1].move_count == 0) and ("b1" in hpos)]
 
         if any(valid_moves):
-            return True
-        raise InvalidMove("Invalid King Move")
+            if valid_moves[1]:
+                return True, "queen_side_castle"
+            elif valid_moves[2]:
+                return True, "king_side_castle"
+            else:
+                return True, "normal"
+        else:
+            return False, ""
 
 
 class Queen(Piece):
-    def __init__(self, square, color):
-        Piece.__init__(self, square, color)
+    def __init__(self, square, color, side):
+        Piece.__init__(self, square, color, side)
 
-    def _move(self, paths, to, dx, dy):
-        valid_V_locations = map(lambda x: x.location, paths["V"])
-        valid_D_locations = map(lambda x: x.location, paths["D"])
-        valid_H_locations = map(lambda x: x.location, paths["H"])
-
-        if (to in valid_H_locations) or (to in valid_H_locations) or (to in valid_H_locations):
-            return True
-        raise InvalidMove("InvalidQueenMove")
+    def _move(self, paths, to, dx, dy, vpos, dpos, hpos, lpos):
+        if (to in hpos) or (to in vpos) or (to in hpos):
+            return True, "normal"
+        else:
+            return False, ""
 
 
 class Board(dict):
     #~ Makes a dict of all the free spaces for the board
-    free_spaces = {letter:{number:Space(letter+number) for number in "12345678"} for letter in "abcdefgh"}
+    free_spaces = {letter:{number:Square(letter+number) for number in "12345678"} for letter in "abcdefgh"}
 
     def __init__(self, sub_dict=False):
 
@@ -274,7 +301,7 @@ class Board(dict):
             self.sub = sub_dict
         else:
             self.sub = self.free_spaces
-
+        
         #~ Handle the nested dicts
         for key in self.sub.keys():
             if (type(self.sub[key]) is dict):
@@ -284,8 +311,10 @@ class Board(dict):
 
         #~ We're at the last file, we can now link our squares together.
         if key == "h":
-            self._link_spaces()
+            self.last_moved = None
+            self._link_squares()
             self._setup_pieces()
+            
 
     def show(self):
         #~ "┌ ┐ └ ┘ ┬ ┴ ├ ┤ ─ │ ┼"
@@ -308,26 +337,48 @@ class Board(dict):
         letter, number = location
         return self[letter][number]
 
-    def _link_spaces(self):
+    def _link_squares(self):
         for letter in self.keys():
             for number in self[letter].keys():
                 self[letter][number].init(self)
 
     def _setup_pieces(self):
         lineup = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
-        for piece, letter in zip(lineup, "abcdefgh"):
+        white = []
+        black = []
+        for piece, letter, side in zip(lineup, "abcdefgh", "qqqqkkkk"):
             for number, color in zip("18", "wb"):
                 square = self.get(letter+number)
-                square.piece = piece(square, color)
+                player = piece(square, color, side)
+                if color == "w":
+                    white.append(player)
+                else:
+                    black.append(player)
+                square.piece = player
+                
             for number, color in zip("27", "wb"):
                 square = self.get(letter+number)
-                square.piece = Pawn(square, color)
-
+                player = Pawn(square, color, side)
+                if color == "w":
+                    white.append(player)
+                else:
+                    black.append(player)
+                square.piece = player
+        self._assign_team(white, black)
+    
+    def _assign_team(self, white, black):
+        for letter in "abcdefgh":
+            for number in "12":
+                self.get(letter + number).piece.team = white
+            for number in "78":
+                self.get(letter + number).piece.team = black
 
 if __name__ == "__main__":
     b = Board()
-    b.a2.piece = Pawn(b.a2, 'w')
 
     b.a2.move("a4")
-    b.b7.move("b5")
+    b.a4.move("a5")
+    b.a5.move("a6")
+    b.b7.move("b6")
+    
 
--- newBoard_tests.py
+++ newBoard_tests.py
@@ -7,19 +7,17 @@ class TestWhitePawnMoves(TestCase):
     
     def setUp(self):
         self.b = Board()
-        self.b.a2.piece = Pawn(self.b.a2, 'w')
-        
-    def test_pawn_move_one_N(self):
-        self.assertEqual(self.b.a2.move("a3"), True)
 
-    def test_pawn_move_two_N_from_start(self):
-        self.assertEqual(self.b.a2.move("a4"), True)
+    def tearDown(self):
+        del self.b
 
-    def test_pawn_move_two_N_after_already_moved(self):
-        self.b.a2.piece.move_count = 1
-        self.assertRaises(InvalidMove, self.b.a2.move, "a4")
+    def test_first_move_one_space(self):
+        self.assertEqual(self.b.a2.move("a3"), (True, "normal"))
+        self.assertEqual(self.b.a3.move("a4"), (True, "normal"))
 
-    def test_pawn_move_three_N(self):
-        self.assertRaises(InvalidMove, self.b.a2.move, "a5")
+    def test_first_move_two_spaces(self):
+        self.assertEqual(self.b.a2.move("a4"), (True, "normal"))
+        self.assertEqual(self.b.a4.move("a6"), (False, ""))
+        self.assertEqual(self.b.a4.move("a5"), (True, "normal"))
 
 main()
